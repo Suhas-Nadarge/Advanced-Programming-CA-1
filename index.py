608804ad70e0e6d47952eb7c83e19e519b5319c4
Suhas Nadarge <suhas.nadarge@mnx.com>
2021-12-04 23:24:01 +0000

    Calculation changes and final dispaly object changes

--- index.py
+++ index.py
@@ -12,10 +12,10 @@ class Employee:
     resultOutput = dict()
 
     with open ('Employees.txt', 'w') as f:
-        f.write('12345 Green Joe 37 16 1.5 70 700 ,12346 Suhas Reddy 35 16 1.5 70 700')
+        f.write('12345 Green Joe 37 16 3 70 700 ,12346 Suhas Reddy 35 16 1.5 70 700')
         # <StaffID> <LastName> <FirstName> <RegHours> <HourlyRate> <OTMultiple> <TaxCredit> <StandardBand>
     with open('Hours.txt','w') as f:
-        f.write('12345 31/10/2021 42 ,12346 31/10/2021 40')
+        f.write('12345 31/10/2021 42 ,12346 31/10/2021 40 ,12345 30/10/2021 63')
         # <StaffID> <Date> <HoursWorked>
 
 
@@ -59,10 +59,15 @@ class Employee:
 
 
     def computePayment(self,date,hours_worked):
-          print(self.currentEmpDetails['HourlyRate'],self.currentEmpDetails['RegHours']) 
+        #   print(self.currentEmpDetails)
         # Taken from https://techvidvan.com/tutorials/ternary-operator-in-python modified by me(for conditional statements)
-          self.regularPay = float(self.currentEmpDetails['HourlyRate'])*float(self.currentEmpDetails['RegHours'] if float(hours_worked) > float(self.currentEmpDetails['RegHours']) else hours_worked )
-          self.overtimeRate = float(self.currentEmpDetails['HourlyRate'])*float(self.currentEmpDetails['OTMultiple'])
+          self.name = self.currentEmpDetails['FirstName']+''+self.currentEmpDetails['LastName']
+          self.date = date
+          self.regularHoursWorked = float(self.currentEmpDetails['RegHours'] if float(hours_worked) > float(self.currentEmpDetails['RegHours']) else hours_worked) 
+          self.regularRate = self.currentEmpDetails['HourlyRate']
+          self.regularPay = float(self.regularRate)*self.regularHoursWorked
+          
+          self.overtimeRate = float(self.regularRate)*float(self.currentEmpDetails['OTMultiple'])
           
           if float(hours_worked) > float(self.currentEmpDetails['RegHours']):
               self.overtimeHoursWorked = float(hours_worked) - float(self.currentEmpDetails['RegHours'])
@@ -70,35 +75,45 @@ class Employee:
               self.overtimeHoursWorked = 0
           self.overtimePay = self.overtimeHoursWorked*self.overtimeRate
           self.grossPay = self.regularPay + self.overtimePay
+          self.standardRatePay = float(self.currentEmpDetails['StandardBand'])
           if self.grossPay > float(self.currentEmpDetails['StandardBand']):
               self.higherRatePay = self.grossPay - float(self.currentEmpDetails['StandardBand'])
           else:
               self.higherRatePay = 0
 
+        # Taxes calculation starts
           self.higherTax = round(0.4*self.higherRatePay, 2)
           self.standardTax = 0.2*float(self.currentEmpDetails['StandardBand'])
-          
           self.totalTax = self.higherTax+self.standardTax
+        # Taxes calculation ends
+
+        # Deduction calculation starts
           self.netDeduction = self.totalTax-int(self.currentEmpDetails['TaxCredit'])
           self.netDeduction = round(self.netDeduction,2)
           self.netPay = self.grossPay-self.netDeduction
+        # Deduction calculation ends  
+        
           self.displayData()
 
     # This method will display calculated data
     def displayData(self):
         # deleting properties which we created from txt file for iterating purpose
-        delattr(self, 'currentEmpDetails')
-        delattr(self, 'currentHoursDetails')
+        # delattr(self, 'currentEmpDetails')
+        # delattr(self, 'currentHoursDetails')
 
         for item in vars(self):
             # return dictionary object in key:value pair
-            self.resultOutput[item] = vars(self)[item]
+            # Exclude the current objects which we created from txt files for calculation purpose, as we don't need it in output
+            if item not in ['currentEmpDetails','currentHoursDetails']:
+             self.resultOutput[item] = vars(self)[item]
+
+
         print(self.resultOutput)
 
 
-    # def computeAllPayment():
-    #     print()
-
+    def computeAllPayment():
+        print()
+ 
 # Step-1:Method to read text files and construct object for Employee and Hours details
 setData = Employee.constuctEmpHoursObject("Employees.txt","Hours.txt")
 
@@ -107,6 +122,10 @@ suhasObj = Employee('12345')
 
 # Step-3:Call compute payment method by passing date and hours worked
 suhasObj.computePayment('31/10/2021', 42)
+suhasObj.computePayment('30/10/2021', 63)
+
+# suhasObj.computeAllPayment()
+
 
 
 
